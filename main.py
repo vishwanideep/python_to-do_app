94fd80092dfb2436f1c8d5490a76ec7661efb673
Kumar Vishwanideep <kumar.vishwanideep@laerdal.com>
2024-04-27 00:21:34 +0530

    New Changes

--- main.py
+++ main.py
@@ -3,16 +3,25 @@
 todos =[]
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show/display, edit or exit: ").strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a To-do Activity: ")
             todos.append(todo)
-        case 'show':
-            print(todos)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'edit':
+            number = int(input("Number of the to-do item"))
+            number = number-1
+            new_todo = input("Enter a new to-do:")
+            todos[number] = new_todo
         case 'exit':
             break
+        case x:
+            print("Kya kar raha hai bhai tu?")
 
 print('bye')
 
